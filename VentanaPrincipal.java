b27389af91bc4e9f8dd4d58099d7dda77bc6fc7f
m4st3rair <37892129+m4st3rair@users.noreply.github.com>
2018-03-29 19:35:24 -0600

    commit de actualizacion inicio
    actualiza ventanas principales para comenzar el desarrollo logico

--- VentanaPrincipal.java
+++ VentanaPrincipal.java
@@ -3,46 +3,90 @@ package bolasdb;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class VentanaPrincipal extends JFrame{
+public class VentanaPrincipal extends JFrame implements ActionListener{
     private JPanel panelPrincipal, panelMenu;
-    private JButton inicio, buscar, productos, clientes, cotizaciones, recordatorios, ayuda;
+    private JButton[] botoneraMenu;
     
     
     public VentanaPrincipal(){
         super("Creativos");
+        //[ancho][largo]
+        //700*60 panel menu
+        //700* 640 panel Principal
+        
+        
+        
         this.setSize(1300, 700);
-        this.setResizable(false);
-        this.setVisible(true);
+        this.setResizable(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panelMenu= new JPanel();
+        
         panelMenu.setBackground(Color.black);
         panelPrincipal=new JPanel();
         panelPrincipal.setBackground(Color.white);
         
-        this.setLayout(new BorderLayout());
-        this.add(panelMenu, BorderLayout.WEST);
-        this.add(panelPrincipal, BorderLayout.CENTER);
-        inicio= new JButton("Inicio");
-        buscar= new JButton("Buscar");
-        productos= new JButton("Productos");
-        clientes= new JButton("Clientes");
-        cotizaciones= new JButton("Cotizaciones");
-        recordatorios= new JButton("Recordatorios");
-        ayuda= new JButton("Ayuda");
+        botoneraMenu= new JButton[7];
+        
+        
+        for (int i = 0; i < 7; i++) {
+            botoneraMenu[i]= new JButton();
+            botoneraMenu[i].addActionListener(this);
+            botoneraMenu[i].setActionCommand(""+i);
+            
+            botoneraMenu[i].setBackground(Color.white);
+            ImageIcon imag= new ImageIcon("D:\\iconosDB\\"+(i+1)+".png");
+            Icon icono = new ImageIcon(imag.getImage().getScaledInstance(40, 40, Image.SCALE_DEFAULT));
+            botoneraMenu[i].setIcon(icono);
+            panelMenu.add(botoneraMenu[i]);
+            //botoneraMenu[i].setBounds(50, 50, 5, 5+(50*i));
+            
+        }
         
         
         
         
+        
+
+        
+        
+        
+        
+        
+        this.setLayout(null);
+        panelMenu.setBounds(0, 0, 90, 700);
+        panelPrincipal.setBounds(90, 0, 1200, 700);
+        this.add(panelMenu);
+        this.add(panelPrincipal);
+        
+        
+        panelMenu.setLayout(new FlowLayout());
+        
+        //panelMenu.repaint();
+        
+        
+        this.setVisible(true);
         
         
         
     
-    
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     
