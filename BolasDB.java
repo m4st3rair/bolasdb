a3d4b9ae40b94eb3c376de20771e2e260c606995
javierck6148 <ivaj_misa@hotmail.com>
2018-03-30 15:23:16 -0600

    a ver si ya

--- BolasDB.java
+++ BolasDB.java
@@ -6,7 +6,9 @@ public class BolasDB {
     public static void main(String[] args) {
     VentanaPrincipal ventanaP = new VentanaPrincipal();
     
-        
+        /*
+			comentario para provar si esta vez si agarra
+        */
         
     
     
